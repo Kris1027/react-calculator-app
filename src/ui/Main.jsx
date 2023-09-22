0ab427257f08e56d454540c318f3cdf72b391c83
Kris1027 <obarzanek.work@gmail.com>
2023-09-22 19:15:59 +0200

    add function handleDelete

--- src/ui/Main.jsx
+++ src/ui/Main.jsx
@@ -61,16 +61,26 @@ function Main() {
     setCurrentValue('0');
   }
 
+  function handleDelete() {
+    if (operation === '') {
+      setFirstNumber(firstNumber.slice(0, -1));
+      setCurrentValue(firstNumber.slice(0, -1));
+    } else {
+      setSecondNumber(secondNumber.slice(0, -1));
+      setCurrentValue(secondNumber.slice(0, -1));
+    }
+  }
+
   return (
     <main className="flex flex-col gap-y-6">
-      <div className="flex h-20 flex-wrap items-center justify-end rounded-lg bg-indigo-900 px-4 text-5xl font-bold text-white">
+      <div className="flex h-20 flex-wrap items-center justify-end rounded-lg bg-indigo-900 px-4 text-5xl font-bold text-white ">
         {currentValue}
       </div>
       <div className="grid grid-cols-4 gap-4 rounded-lg bg-indigo-900 p-6 text-3xl font-bold">
         <Button onClick={() => handleSetNumbers(7)}>7</Button>
         <Button onClick={() => handleSetNumbers(8)}>8</Button>
         <Button onClick={() => handleSetNumbers(9)}>9</Button>
-        <Button>del</Button>
+        <Button onClick={handleDelete}>del</Button>
         <Button onClick={() => handleSetNumbers(4)}>4</Button>
         <Button onClick={() => handleSetNumbers(5)}>5</Button>
         <Button onClick={() => handleSetNumbers(6)}>6</Button>
@@ -79,7 +89,7 @@ function Main() {
         <Button onClick={() => handleSetNumbers(2)}>2</Button>
         <Button onClick={() => handleSetNumbers(3)}>3</Button>
         <Button onClick={() => handleSetOperations('-')}>-</Button>
-        <Button>.</Button>
+        <Button onClick={() => handleSetNumbers('.')}>.</Button>
         <Button onClick={() => handleSetNumbers(0)}>0</Button>
         <Button onClick={() => handleSetOperations('/')}>/</Button>
         <Button onClick={() => handleSetOperations('*')}>x</Button>
